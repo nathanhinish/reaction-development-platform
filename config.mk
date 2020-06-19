6223a1f59113a3cf459fcc80d8bf313d19ca81c6
Nathan Hinish <nathan@triadinteracitve.com>
2020-06-18 20:41:44 -0700

    remove plugins for nwo

--- config.mk
+++ config.mk
@@ -30,11 +30,8 @@ define SUBPROJECT_REPOS
 https://github.com/reactioncommerce/reaction-hydra.git,reaction-hydra,v3.0.0 \
 https://github.com/nathanhinish/reaction.git,reaction,v3.8.0 \
 https://github.com/reactioncommerce/reaction-identity.git,reaction-identity,v3.1.0 \
-https://github.com/reactioncommerce/reaction-admin.git,reaction-admin,v3.0.0-beta.7 \
-https://github.com/reactioncommerce/example-storefront.git,example-storefront,v3.1.0 \
-https://github.com/nathanhinish/reactioncommerce-api-plugin-payments-paypal.git,api-plugin-payments-paypal,trunk \
-https://github.com/nathanhinish/reactioncommerce-api-plugin-wishlists.git,api-plugin-wishlists,trunk \
-https://github.com/nathanhinish/reactioncommerce-admin-plugin-wishlists.git,admin-plugin-wishlists,trunk
+https://github.com/nathanhinish/reaction-admin.git,reaction-admin,v3.0.0-beta.7 \
+https://github.com/reactioncommerce/example-storefront.git,example-storefront,v3.1.0
 endef
 
 # These are all the plugins that `make clone-api-plugins` will clone.
